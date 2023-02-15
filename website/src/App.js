646f234bf187f2fc1f29631aebde03d2787743d3
ArminGrossmann <armingrossmann1@gmail.com>
2023-02-15 12:29:32 +0100

    #

---
+++ website/src/App.js
@@ -0,0 +1,37 @@
+import logo from './logo.svg';
+import './App.css';
+import data from './mock-data.json'
+import { useState } from 'react';
+
+const App =() => {
+
+  const[contacts, setContacts] = useState(data);
+
+  return (
+    <div className="app-container">
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Adresse</th>
+            <th>Phone Number</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {contacts.map((contact)=>(
+          <tr>
+            <td>{contact.fullName}</td>
+            <td>{contact.address}</td>
+            <td>{contact.phoneNumber}</td>
+            <td>{contact.email}</td>
+          </tr>
+          ))}
+        </tbody>
+      </table>
+
+    </div>
+  );
+}
+
+export default App;
